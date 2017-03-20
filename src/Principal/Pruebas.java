9986dc2432da4cb41410bf337b5212fd48b925b8
Patricio Rodriguez <patricio.rodriguezg@alumnos.usm.cl>
2017-03-20 18:30:28 -0300

    ventana

--- src/Principal/Pruebas.java
+++ src/Principal/Pruebas.java
@@ -4,7 +4,12 @@ public class Pruebas {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Lala???");
+		//System.out.println("Lala???");
+		String a = "oli";
+		String b = "oli.";
+		Arbol tree = new Arbol();
+		Arbol tree2 = new Arbol();
+		System.out.println(tree.getClass().getName());
 	}
 
 }
---
+++ src/Principal/Ventana.java
@@ -0,0 +1,53 @@
+package Principal;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Ventana extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Ventana frame = new Ventana();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Ventana() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		JButton btnOli = new JButton("oli");
+		btnOli.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("Holi!");
+			}
+		});
+		contentPane.add(btnOli, BorderLayout.NORTH);
+	}
+
+}
